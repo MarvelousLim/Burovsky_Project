a2f0c4abb1b2a81783d4871bdbb3e72b7c151102
Limen2402 <Limen2402@gmail.com>
2018-11-26 22:10:55 +0300

    test file done

--- Burovski_Project/test.py
+++ Burovski_Project/test.py
@@ -2,8 +2,20 @@ import numpy as np
 from scipy import integrate
 import matplotlib.pyplot as plt
 
-def pend_deriv(t, y):
+y_0 = 1
+threshold = 0.01
+t = np.linspace(0.0, 10.0, 100)
+delta_t = t[1]
 
+def derv(y, t):
+    global delta_t
+    dydt = -y
+    if (y < threshold):
+        dydt += 10 / delta_t
     return dydt 
 
-sol = integrate.odeint(pend_deriv, t, y_0) #global variable
+sol = integrate.odeint(derv, y_0, t) #global variable
+
+plt.plot(t, sol, 'o')
+plt.grid()
+plt.show()
