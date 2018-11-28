c06b19ba1d9df6b12754c6ee2b3a62821f10c006
Limen2402 <Limen2402@gmail.com>
2018-11-28 14:14:51 +0300

    RHS another try

--- Burovski_Project/test.py
+++ Burovski_Project/test.py
@@ -2,17 +2,21 @@ import numpy as np
 from scipy import integrate
 import matplotlib.pyplot as plt
 
-y_0 = 1
-threshold = 0.01
-t = np.linspace(0.0, 10.0, 100)
-delta_t = t[1]
+y_0 = 3
+threshold_low = 1
+threshold_high = 2
+t = np.linspace(0.0, 10.0, 1000)
+direction = -1
 
 def derv(y, t):
-    global delta_t
-    dydt = -y
-    if (y < threshold):
-        dydt += 10 / delta_t
-    return dydt 
+    global direction
+
+    if (y < threshold_low):
+        direction = 1
+    if (y > threshold_high):
+        direction = -1
+
+    return direction
 
 sol = integrate.odeint(derv, y_0, t) #global variable
 
